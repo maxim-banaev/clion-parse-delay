83a8de446e5a9436195f4e9985625db50e21604e
Maxim Banaev <Maxim.Banaev@jetbrains.com>
2019-09-05 18:33:43 +0300

    Initial commit

---
+++ long_parse_hader.h
@@ -0,0 +1,60 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+// syntetich parse dalay
+[[cidr_parse_delay 100]] int a;
+[[cidr_parse_delay 100]] int b;
+[[cidr_parse_delay 100]] int c;
+[[cidr_parse_delay 100]] int d;
+[[cidr_parse_delay 100]] int e;
+[[cidr_parse_delay 100]] int f;
+[[cidr_parse_delay 100]] int g;
+[[cidr_parse_delay 100]] int h;
+[[cidr_parse_delay 100]] int i;
+[[cidr_parse_delay 100]] int j;
+[[cidr_parse_delay 100]] int k;
+[[cidr_parse_delay 100]] int l;
+[[cidr_parse_delay 100]] int m;
+[[cidr_parse_delay 100]] int n;
+[[cidr_parse_delay 100]] int o;
+[[cidr_parse_delay 100]] int p;
+[[cidr_parse_delay 100]] int q;
+[[cidr_parse_delay 100]] int r;
+[[cidr_parse_delay 100]] int s;
+[[cidr_parse_delay 100]] int t;
+[[cidr_parse_delay 100]] int u;
+[[cidr_parse_delay 100]] int w;
+[[cidr_parse_delay 100]] int x;
+[[cidr_parse_delay 100]] int y;
+[[cidr_parse_delay 100]] int z;
+
+[[cidr_parse_delay 100]] int a1;
+[[cidr_parse_delay 100]] int b1;
+[[cidr_parse_delay 100]] int c1;
+[[cidr_parse_delay 100]] int d1;
+[[cidr_parse_delay 100]] int e1;
+[[cidr_parse_delay 100]] int f1;
+[[cidr_parse_delay 100]] int g1;
+[[cidr_parse_delay 100]] int h1;
+[[cidr_parse_delay 100]] int i1;
+[[cidr_parse_delay 100]] int j1;
+[[cidr_parse_delay 100]] int k1;
+[[cidr_parse_delay 100]] int l1;
+[[cidr_parse_delay 100]] int m1;
+[[cidr_parse_delay 100]] int n1;
+[[cidr_parse_delay 100]] int o1;
+[[cidr_parse_delay 100]] int p1;
+[[cidr_parse_delay 100]] int q1;
+[[cidr_parse_delay 100]] int r1;
+[[cidr_parse_delay 100]] int s1;
+[[cidr_parse_delay 100]] int t1;
+[[cidr_parse_delay 100]] int u1;
+[[cidr_parse_delay 100]] int w1;
+[[cidr_parse_delay 100]] int x1;
+[[cidr_parse_delay 100]] int y1;
+[[cidr_parse_delay 100]] int z1;
+
+
+[[__cidr_parse_delay 2000]] int number = 22;
+
+#endif // HEADER_H
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "long_parse_hader.h"
+#include <iostream>
+
+int main() {
+  number = 5;
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+}
